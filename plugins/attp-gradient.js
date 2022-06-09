936f77329c4e78e07af09be5f2a8e20e2d240238
AyGemuy <91945119+AyGemuy@users.noreply.github.com>
2022-06-09 19:40:59 +0800

    x
    x

--- plugins/attp-gradient.js
+++ plugins/attp-gradient.js
@@ -1,80 +1,81 @@
 import { sticker } from '../lib/sticker.js'
 let handler = async(m, { conn, text, args, usedPrefix, command }) => {
+let teks = text ? text : m.quoted && m.quoted.text ? m.quoted.text : m.text
 
 if (!text) return m.reply(`Example : ${usedPrefix + command} teks`)
 if (!args[0]) return m.reply(`Example : ${usedPrefix + command} teks`)
 
 if (command == 'hartacustom') {
-    let stiker = await sticker(null, global.API(`https://api.lolhuman.xyz/api/hartacustom?apikey=9b817532fadff8fc7cb86862&text=${args[0]}`), global.packname, global.author)
+    let stiker = await sticker(null, global.API(`https://api.lolhuman.xyz/api/hartacustom?apikey=9b817532fadff8fc7cb86862&text=${teks}`), global.packname, global.author)
     if (stiker) return conn.sendFile(m.chat, stiker, 'sticker.webp', '', m)
     throw stiker.toString()
 }
 if (command == 'attp1') {
-    let stiker = await sticker(null, global.API(`https://api.lolhuman.xyz/api/attp?apikey=9b817532fadff8fc7cb86862&text=${args[0]}`), global.packname, global.author)
+    let stiker = await sticker(null, global.API(`https://api.lolhuman.xyz/api/attp?apikey=9b817532fadff8fc7cb86862&text=${teks}`), global.packname, global.author)
     if (stiker) return conn.sendFile(m.chat, stiker, 'sticker.webp', '', m)
     throw stiker.toString()
 }
 if (command == 'attp2') {
-    let stiker = await sticker(null, global.API(`https://api.lolhuman.xyz/api/attp2?apikey=9b817532fadff8fc7cb86862&text=${args[0]}`), global.packname, global.author)
+    let stiker = await sticker(null, global.API(`https://api.lolhuman.xyz/api/attp2?apikey=9b817532fadff8fc7cb86862&text=${teks}`), global.packname, global.author)
     if (stiker) return conn.sendFile(m.chat, stiker, 'sticker.webp', '', m)
     throw stiker.toString()
 }
 
 if (command == 'ttp6') {
-    let stiker = await sticker(null, global.API(`https://api.lolhuman.xyz/api/ttp6?apikey=9b817532fadff8fc7cb86862&text=${args[0]}`), global.packname, global.author)
+    let stiker = await sticker(null, global.API(`https://api.lolhuman.xyz/api/ttp6?apikey=9b817532fadff8fc7cb86862&text=${teks}`), global.packname, global.author)
     if (stiker) return conn.sendFile(m.chat, stiker, 'sticker.webp', '', m)
     throw stiker.toString()
 }
 if (command == 'ttp5') {
-    let stiker = await sticker(null, global.API(`https://api.lolhuman.xyz/api/ttp5?apikey=9b817532fadff8fc7cb86862&text=${args[0]}`), global.packname, global.author)
+    let stiker = await sticker(null, global.API(`https://api.lolhuman.xyz/api/ttp5?apikey=9b817532fadff8fc7cb86862&text=${teks}`), global.packname, global.author)
     if (stiker) return conn.sendFile(m.chat, stiker, 'sticker.webp', '', m)
     throw stiker.toString()
 }
 if (command == 'ttp4') {
-    let stiker = await sticker(null, global.API(`https://api.lolhuman.xyz/api/ttp4?apikey=9b817532fadff8fc7cb86862&text=${args[0]}`), global.packname, global.author)
+    let stiker = await sticker(null, global.API(`https://api.lolhuman.xyz/api/ttp4?apikey=9b817532fadff8fc7cb86862&text=${teks}`), global.packname, global.author)
     if (stiker) return conn.sendFile(m.chat, stiker, 'sticker.webp', '', m)
     throw stiker.toString()
 }
 if (command == 'ttp3') {
-    let stiker = await sticker(null, global.API(`https://api.lolhuman.xyz/api/ttp3?apikey=9b817532fadff8fc7cb86862&text=${args[0]}`), global.packname, global.author)
+    let stiker = await sticker(null, global.API(`https://api.lolhuman.xyz/api/ttp3?apikey=9b817532fadff8fc7cb86862&text=${teks}`), global.packname, global.author)
     if (stiker) return conn.sendFile(m.chat, stiker, 'sticker.webp', '', m)
     throw stiker.toString()
 }
 if (command == 'ttp2') {
-    let stiker = await sticker(null, global.API(`https://api.lolhuman.xyz/api/ttp2?apikey=9b817532fadff8fc7cb86862&text=${args[0]}`), global.packname, global.author)
+    let stiker = await sticker(null, global.API(`https://api.lolhuman.xyz/api/ttp2?apikey=9b817532fadff8fc7cb86862&text=${teks}`), global.packname, global.author)
     if (stiker) return conn.sendFile(m.chat, stiker, 'sticker.webp', '', m)
     throw stiker.toString()
 }
 if (command == 'ttp1') {
-    let stiker = await sticker(null, global.API(`https://api.lolhuman.xyz/api/ttp?apikey=9b817532fadff8fc7cb86862&text=${args[0]}`), global.packname, global.author)
+    let stiker = await sticker(null, global.API(`https://api.lolhuman.xyz/api/ttp?apikey=9b817532fadff8fc7cb86862&text=${teks}`), global.packname, global.author)
     if (stiker) return conn.sendFile(m.chat, stiker, 'sticker.webp', '', m)
     throw stiker.toString()
 }
 
 if (command == 'ttpg') {
-    let stiker = await sticker(null, global.API(`https://violetics.pw/api/canvas/ttp-gradient?apikey=beta&text=${text}`), global.packname, global.author)
+    let stiker = await sticker(null, global.API(`https://violetics.pw/api/canvas/ttp-gradient?apikey=beta&text=${teks}`), global.packname, global.author)
     if (stiker) return conn.sendFile(m.chat, stiker, 'sticker.webp', '', m)
     throw stiker.toString()
 }
 if (command == 'attpg') {
-    let stiker = await sticker(null, global.API(`https://violetics.pw/api/canvas/attp-gradient?apikey=beta&text=${text}`), global.packname, global.author)
+    let stiker = await sticker(null, global.API(`https://violetics.pw/api/canvas/attp-gradient?apikey=beta&text=${teks}`), global.packname, global.author)
     if (stiker) return conn.sendFile(m.chat, stiker, 'sticker.webp', '', m)
     throw stiker.toString()
 }
 if (command == 'attpg2') {
-    let stiker = await sticker(null, global.API(`https://violetics.pw/api/canvas/attp-gradient2?apikey=beta&text=${text}`), global.packname, global.author)
+    let stiker = await sticker(null, global.API(`https://violetics.pw/api/canvas/attp-gradient2?apikey=beta&text=${teks}`), global.packname, global.author)
     if (stiker) return conn.sendFile(m.chat, stiker, 'sticker.webp', '', m)
     throw stiker.toString()
 }
 if (command == 'quotex') {
-    let images = `https://violetics.pw/api/canvas/quotes?apikey=beta&text=${text}&author=HinataMd`
+    let images = `https://violetics.pw/api/canvas/quotes?apikey=beta&text=${teks}&author=HinataMd`
         let buttons = [
-                    {buttonId: `/quotes ${text}`, buttonText: {displayText: '➡️➡️Next Quotes➡️➡️'}, type: 1}
+                    {buttonId: `/quotes ${teks}`, buttonText: {displayText: '➡️➡️Next Quotes➡️➡️'}, type: 1}
                 ]
                 let buttonMessage = {
                     image: { url: images },
                     caption: `*⎔┉━「 ${command} 」━┉⎔*
-🤠 *Query* : *${text}*
+🤠 *Query* : *${teks}*
 `,
                     footer: conn.user.name,
                     buttons: buttons,
@@ -83,14 +84,14 @@ if (command == 'quotex') {
                 conn.sendMessage(m.chat, buttonMessage, { quoted: m })
 }
 if (command == 'tahta') {
-let images = `https://violetics.pw/api/jimp/tahta?apikey=beta&text=${text}`
+let images = `https://violetics.pw/api/jimp/tahta?apikey=beta&text=${teks}`
         let buttons = [
-                    {buttonId: `/tahta ${text}`, buttonText: {displayText: '➡️➡️Next Quotes➡️➡️'}, type: 1}
+                    {buttonId: `/tahta ${teks}`, buttonText: {displayText: '➡️➡️Next Quotes➡️➡️'}, type: 1}
                 ]
                 let buttonMessage = {
                     image: { url: images },
                     caption: `*⎔┉━「 ${command} 」━┉⎔*
-🤠 *Query* : *${text}*
+🤠 *Query* : *${teks}*
 `,
                     footer: conn.user.name,
                     buttons: buttons,
