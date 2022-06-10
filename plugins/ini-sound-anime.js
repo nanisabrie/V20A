ab86c4618527105b863da3b4b8486c156a008d6f
AyGemuy <91945119+AyGemuy@users.noreply.github.com>
2022-06-10 18:02:38 +0800

    x
    x

--- plugins/ini-sound-anime.js
+++ plugins/ini-sound-anime.js
@@ -1,8 +1,8 @@
 import fetch from 'node-fetch'
 
-let handler = async(m, { conn, usedPrefix, text, args, command, noPrefix }) => {
+let handler = async(m, { conn, usedPrefix, text, args, command }) => {
 
-if (noPrefix == 'ara') {
+if (command == 'ara') {
 let vn = `https://raw.githubusercontent.com/andgyk/anime-soundboard/main/public/audio/ara-ara.mp3`
 await conn.sendFile(m.chat, vn, 'ara.mp3', null, m, true, {
 type: 'audioMessage', 
@@ -10,7 +10,7 @@ ptt: true
 })
 }
 
-if (noPrefix == 'ganbare') {
+if (command == 'ganbare') {
 let vn = `https://raw.githubusercontent.com/andgyk/anime-soundboard/main/public/audio/ganbare-ganbare-senpai.mp3`
 await conn.sendFile(m.chat, vn, 'ganbare.mp3', null, m, true, {
 type: 'audioMessage', 
@@ -18,7 +18,7 @@ ptt: true
 })
 }
 
-if (noPrefix == 'konichiwa') {
+if (command == 'konichiwa') {
 let vn = `https://raw.githubusercontent.com/andgyk/anime-soundboard/main/public/audio/hashira-konichiwa.mp3`
 await conn.sendFile(m.chat, vn, 'konichiwa.mp3', null, m, true, {
 type: 'audioMessage', 
@@ -26,7 +26,7 @@ ptt: true
 })
 }
 
-if (noPrefix == 'nani') {
+if (command == 'nani') {
 let vn = `https://raw.githubusercontent.com/andgyk/anime-soundboard/main/public/audio/nani.mp3`
 await conn.sendFile(m.chat, vn, 'nani.mp3', null, m, true, {
 type: 'audioMessage', 
@@ -34,7 +34,7 @@ ptt: true
 })
 }
 
-if (noPrefix == 'rikka') {
+if (command == 'rikka') {
 let vn = `https://raw.githubusercontent.com/andgyk/anime-soundboard/main/public/audio/rikka-ow.mp3`
 await conn.sendFile(m.chat, vn, 'rikka.mp3', null, m, true, {
 type: 'audioMessage', 
@@ -42,7 +42,7 @@ ptt: true
 })
 }
 
-if (noPrefix == 'ultra') {
+if (command == 'ultra') {
 let vn = `https://raw.githubusercontent.com/andgyk/anime-soundboard/main/public/audio/ultra-instinct.mp3`
 await conn.sendFile(m.chat, vn, 'ultra.mp3', null, m, true, {
 type: 'audioMessage', 
@@ -50,7 +50,7 @@ ptt: true
 })
 }
 
-if (noPrefix == 'ahh') {
+if (command == 'ahh') {
 let vn = `https://raw.githubusercontent.com/andgyk/anime-soundboard/main/public/audio/yemete-kudasai-ah.mp3`
 await conn.sendFile(m.chat, vn, 'ahh.mp3', null, m, true, {
 type: 'audioMessage', 
@@ -58,7 +58,7 @@ ptt: true
 })
 }
 
-if (noPrefix == 'yemete') {
+if (command == 'yemete') {
 let vn = `https://raw.githubusercontent.com/andgyk/anime-soundboard/main/public/audio/yemete-kudasai.mp3`
 await conn.sendFile(m.chat, vn, 'yemete.mp3', null, m, true, {
 type: 'audioMessage', 
@@ -66,7 +66,7 @@ ptt: true
 })
 }
 
-if (noPrefix == 'yuno') {
+if (command == 'yuno') {
 let vn = `https://raw.githubusercontent.com/andgyk/anime-soundboard/main/public/audio/yuno-yukki.mp3`
 await conn.sendFile(m.chat, vn, 'yuno.mp3', null, m, true, {
 type: 'audioMessage', 
@@ -75,6 +75,6 @@ ptt: true
 }
 
 }
-handler.noPrefix = ['ara', 'ganbare', 'konichiwa', 'nani', 'rikka', 'ultra', 'ahh', 'yemete', 'yuno']
+handler.command = ['ara', 'ganbare', 'konichiwa', 'nani', 'rikka', 'ultra', 'ahh', 'yemete', 'yuno']
 
 export default handler
