8e003e7c84bb8e8a9cda37c96a75d85e5ed89c80
AyGemuy <91945119+AyGemuy@users.noreply.github.com>
2022-06-11 21:18:18 +0800

    New
    New

--- plugins/get-icons.js
+++ plugins/get-icons.js
@@ -1,12 +1,13 @@
 import fetch from 'node-fetch'
 
 let handler = async(m, { conn, groupMetadata, usedPrefix, text, args, command }) => {
-
 if (!text) throw `Contoh penggunaan ${usedPrefix}${command} win8 home`
 let thm = args[0]
 let text1 = args.slice(1).join(' ')
-
-await conn.sendFile(m.chat, `https://api.icons8.com/api/iconsets/v3/search?term=${text1}&amount=1&offset=0&platform=${thm}&language=en-US&exact_amount=1`, 'sticker.webp', '', m)
+let res = await fetch(`https://api.icons8.com/api/iconsets/v3/search?term=${text1}&amount=1&offset=0&platform=${thm}&language=en-US&exact_amount=1`)
+let x = await res.json()
+let o = x.result
+await conn.sendFile(m.chat, o.share.url, 'sticker.webp', '', m)
 }
 handler.command = /^(geticons)$/i
 
