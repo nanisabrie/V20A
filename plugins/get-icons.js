6c073458795343afc387e9dc30792bbe9006c479
AyGemuy <91945119+AyGemuy@users.noreply.github.com>
2022-06-12 09:33:06 +0800

    New
    New

--- plugins/get-icons.js
+++ plugins/get-icons.js
@@ -7,13 +7,12 @@ if (!text) throw `Contoh penggunaan ${usedPrefix}${command} emoji|2`
 let urut = text.split`|`
   let text1 = urut[0]
   let text2 = urut[1]
+  
     let gas = await fetch(`https://g.tenor.com/v1/search?q=${text1}&key=LIVDSRZULELA&limit=${text2}`)
     let json = await gas.json()
-    m.reply(`*Total stiker:* ${json.result.media.length}
-*Estimasi selesai:* ${json.result.media.length * 10} detik`.trim())
 
-    for (let i = 0; i < json.result.stickers.length; i++) {
-        let fileId = json.result.media[i].url
+    for (let i = 0; i < json.results.media.length; i++) {
+        let fileId = json.results.media[i].url
         let stiker = await sticker(false, fileId, global.packname, global.author)
         await conn.sendFile(m.chat, stiker, null, { asSticker: true })
         await delay(1500)
