55b660bd7d34f81902e7565f6217985bebe3b271
AyGemuy <91945119+AyGemuy@users.noreply.github.com>
2022-06-12 11:33:02 +0800

    New
    New

--- plugins/get-icons.js
+++ plugins/get-icons.js
@@ -1,5 +1,4 @@
 import fetch from 'node-fetch'
-import { webp2mp4 } from '../lib/webp2mp4.js'
 
 let handler = async(m, { conn, usedPrefix, text, args, command }) => {
 if (!text) throw `Contoh penggunaan ${usedPrefix}${command} emoji|2`
@@ -10,10 +9,10 @@ let urut = text.split`|`
   
     let gas = await fetch(`https://g.tenor.com/v1/search?q=${text1}&key=LIVDSRZULELA&limit=${text2}`)
     let json = await gas.json()
+    let ter = json.results
 
-    for (let i = 0; i < json.results; i++) {
-        let fileId = json.results[i].url
-        let out = await webp2mp4(fileId)
+    for (let i = 0; i < ter.length; i++) {
+        let out = ter[i].url
         await conn.sendMessage(m.chat, { video: { url: out }, caption: "Nih!", gifPlayback: true })
     }
 }
