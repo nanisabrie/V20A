7743cdc7b70678667b5a29d6ad823b0a263c01f1
AyGemuy <91945119+AyGemuy@users.noreply.github.com>
2022-06-12 12:43:52 +0800

    New
    New

--- plugins/get-icons.js
+++ plugins/get-icons.js
@@ -9,12 +9,11 @@ let urut = text.split`|`
   
     let gas = await fetch(`https://g.tenor.com/v1/search?q=${text1}&key=LIVDSRZULELA&limit=${text2}`)
     let json = await gas.json()
-    let ter = json.results
+    let ter = json.results.media
 
     for (let i = 0; i < ter.length; i++) {
-        let out = ter[i].url
-        let so = await conn.sendMessage(m.chat, { video: { url: out }, caption: "Nih!", gifPlayback: false })
-        m.reply(so, '.s')
+        let out = ter[i].gif.url
+        await conn.sendMessage(m.chat, { video: { url: out }, caption: ".s", gifPlayback: false })
     }
 }
 
