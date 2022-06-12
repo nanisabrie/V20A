29db8461aac5d9c67d1b04cf08610bedaf970f94
AyGemuy <91945119+AyGemuy@users.noreply.github.com>
2022-06-12 10:02:48 +0800

    New
    New

--- plugins/get-icons.js
+++ plugins/get-icons.js
@@ -1,6 +1,4 @@
 import fetch from 'node-fetch'
-import { sticker } from '../lib/sticker.js'
-import { webp2mp4 } from '../lib/webp2mp4.js'
 
 let handler = async(m, { conn, usedPrefix, text, args, command }) => {
 if (!text) throw `Contoh penggunaan ${usedPrefix}${command} emoji|2`
@@ -14,15 +12,10 @@ let urut = text.split`|`
 
     for (let i = 0; i < json.results; i++) {
         let fileId = json.results[i].url
-        let out = await webp2mp4(fileId)
-        await conn.sendFile(m.chat, out, 'out.gif', m, false, { mimetype: 'video/gif', thumbnail: Buffer.alloc(0) })
-        await conn.sendFile(m.chat, out, null, { asSticker: true })
-        await delay(1500)
+        await conn.sendFile(m.chat, fileId, 'out.gif', m, false, { mimetype: 'video/gif', thumbnail: Buffer.alloc(0) })
     }
 }
 
 handler.command = /^(gettenor)$/i
 
 export default handler
-
-const delay = time => new Promise(res => setTimeout(res, time))
