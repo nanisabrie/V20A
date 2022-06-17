f1e053fd7442bb49a6ca79adf2ee5c0f2c109a47
nanisabrie <94164611+nanisabrie@users.noreply.github.com>
2022-06-18 00:01:52 +0800

    Update youtube-ytv.js
    P

--- plugins/youtube-ytv.js
+++ plugins/youtube-ytv.js
@@ -1,4 +1,4 @@
-let limit = 10
+let limit = 20
 import fetch from 'node-fetch'
 import { youtubedl, youtubedlv2, youtubedlv3 } from '@bochilteam/scraper';
 let handler = async (m, { conn, args, isPrems, isOwner }) => {
